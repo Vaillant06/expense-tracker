c59374e9e9816d7d578e693225221e27bd78724b
SREENATH G <sreenath2410530@ssn.edu.in>
2025-09-14 09:48:40 +0530

    Updated the responsiveness of the appliaction

--- static/js/editProfileModal.js
+++ static/js/editProfileModal.js
@@ -1,19 +1,17 @@
-// Edit Profile Modal
+// Open and close Edit Profile Modal
 function openEditProfileModal() {
     document.getElementById("editProfileModal").style.display = "block";
 }
+
 function closeEditProfileModal() {
     document.getElementById("editProfileModal").style.display = "none";
 }
 
-// Close modal if user clicks outside content
-window.onclick = function(event) {
+// Close modals when clicking outside
+window.onclick = (event) => {
     const editProfileModal = document.getElementById("editProfileModal");
     const budgetModal = document.getElementById("budgetModal");
-    if (event.target === editProfileModal) {
-        editProfileModal.style.display = "none";
-    }
-    if (event.target === budgetModal) {
-        budgetModal.style.display = "none";
-    }
-}
+
+    if (event.target === editProfileModal) editProfileModal.style.display = "none";
+    if (event.target === budgetModal) budgetModal.style.display = "none";
+};
